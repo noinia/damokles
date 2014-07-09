7d315d315d4d8f9724da63a5a67cfe128dd4fe60
Frank Staals <frank@Shanna.FStaals.net>
2014-07-09 11:29:48 +0200

    Some more functions

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,57 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
 module Main where
 
+import Control.Applicative
 
 import Data.Monoid
+import Data.String
+import Data.Time.Calendar(fromGregorian, diffDays)
 
-import Data.Time.Calendar
+import Data.Time.LocalTime
 
+
+import qualified Data.Time.Calendar as C
 
 --------------------------------------------------------------------------------
 
 newtype UserId = UserId { unUI :: String }
-               deriving (Read,Show,Eq)
+               deriving (Read,Show,Eq,IsString)
 
 
 data Gender = Male | Female
                      deriving (Read,Show,Eq)
 
-newtype Date = Date { unDay :: Day }
-               deriving (Eq,Ord)
+newtype Date = Date { unDay :: C.Day }
+               deriving (Eq,Ord,Show)
+
+type Year  = Integer
+type Month = Int
+type Day   = Int
+
+fromDate             :: Year -> Month -> Day -> Date
+fromDate yyyy mm dd = Date $ fromGregorian yyyy mm dd
 
 
-instance Show Date where
-  show = undefined
+today :: IO Date
+today = Date . localDay . zonedTimeToLocalTime <$> getZonedTime
+
+(Date a) `timeUntil` (Date b) = collectTime $ b `diffDays` a
+  where
+    collectTime t = let years  =               t  `div` 365
+                        t'     =               t  `mod` 365
+                        months = fromInteger $ t' `div` 31
+                        days   = fromInteger $ t' `mod` 31
+                    in (years,months,days)
+-- TODO: We can make this more precise (e.g. taking care of leap-years etc.) by
+-- adding durations/days starting at a, and running until we hit b.
+
+
+
+
+-- instance Show Date where
+--   show = showGregorian . unDay
 
 instance Read Date where
   readsPrec = undefined
@@ -33,6 +62,11 @@ data Person = Person { userId  :: UserId
                      , dueDate :: Date
                      }
               deriving (Read,Show,Eq)
+
+timeLeft   :: Person -> IO (Year,Month,Day)
+timeLeft p = (`timeUntil` dueDate p) <$> today
+
+
 
 
 newtype Height = Height { unH :: Int }
@@ -49,5 +83,9 @@ data ProgressState = MayStill | HasTo | HasStill | HasOnly | IsLate
 
 
 --------------------------------------------------------------------------------
+
+
+users = [ Person "Staals" Male $ fromDate 2015 08 31
+        ]
 
 main = do print "woei"
